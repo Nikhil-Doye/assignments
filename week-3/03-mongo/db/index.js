18a6d540850b51408db43cbd476fc47612395e54
Nikhil-Doye <nikhil.doye@gmail.com>
2025-12-03 14:38:24 -0500

    Refactor admin routes and MongoDB connection: update admin signup and course creation routes to use async/await for better error handling, and modify MongoDB connection string to specify the database name.

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
 mongoose.connect(
-  "mongodb+srv://nikhildoye:Johnnash@cluster0.jk7gnti.mongodb.net/"
+  "mongodb+srv://nikhildoye:Johnnash@cluster0.jk7gnti.mongodb.net/course_selling_app"
 );
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
   // Schema definition here
   username: String,
-  password: String
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
   // Schema definition here
   username: String,
   password: String,
-  purchasedCourses: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Course'
-  }]
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
 
 const CourseSchema = new mongoose.Schema({
@@ -27,15 +29,15 @@ const CourseSchema = new mongoose.Schema({
   title: String,
   description: String,
   imageLink: String,
-  price: Number
+  price: Number,
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
