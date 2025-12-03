1a5291c06404fcd930fb58d41f9daf20ca7191c9
Nikhil-Doye <nikhil.doye@gmail.com>
2025-12-03 17:14:25 -0500

    Implement async handling for user signup and course retrieval: update the POST /signup and GET /courses endpoints to use async/await for improved database interactions and response handling.

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,14 +1,31 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+      message: "User created successfully",
+    });
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const response = await Course.find({});
+
+    res.json({
+        courses: response
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
