7b9747142ae06609cffc60aa32c53bbb9945838e
Nikhil-Doye <nikhil.doye@gmail.com>
2025-12-03 17:20:57 -0500

    Add course purchase functionality: implement POST /courses/:courseId endpoint to allow users to purchase courses and update their purchasedCourses list in the database.

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -30,10 +30,25 @@ router.get('/courses', async (req, res) => {
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    User.updateOne({
+        username: username
+    }, {
+        purchasedCourses:{
+            "$push": courseId
+        }
+    });
+
+    res.json({
+      message: "Course purchased successfully",
+    });
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
+
 });
 
 module.exports = router
